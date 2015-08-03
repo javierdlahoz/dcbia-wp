1a4de7a6ac9ed6e578047fcc6d3bb2c9f2dfd148
JJcodesinDC <josh@innuevodigital.com>
2015-08-03 09:38:09 -0400

    Updates for header set up

--- wp-content/themes/DCBIA/header.php
+++ wp-content/themes/DCBIA/header.php
@@ -35,18 +35,20 @@
               </button>
             </div>
 
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <div class="collapse navbar-collapse hide-desktop" id="bs-example-navbar-collapse-1">
               <ul id="main-nav" class="nav navbar-nav custom-nav">
-                  <li id="home" <?php if($url == "" || $url == "") echo "class='active'"; ?>><a href="/">Home</a></li>
-                  <li id="about" <?php if($url == "about" || $url == "clients" || $url == "staff" || $url == "director")
-                      echo "class='active'"; ?>><a href="/about">About Us</a></li>
-                  <li id="resources"><a href="/resources">Resources</a></li>
-                <li id="blog" <?php if($url == "blog" || $url == "blog") echo "class='active'"; ?>><a href="/blog">Blog</a></li>
+                  <li id="about" <?php if($url == "about" || $url == "about")
+                      echo "class='active'"; ?>><a href="/about">ABOUT</a>
+                  </li>
+                  <li id="join"><a href="/join">JOIN</a></li>
+                <li id="advocacy" <?php if($url == "advocacy" || $url == "advocacy") echo "class='active'"; ?>><a href="/advocacy">ADVOCACY</a></li>
                   <li id="donate" <?php if($url == "donate" || $url == "donate") echo "class='active'"; ?>><a href="/donate">Donate</a></li>
 
                   <li id="contact" <?php if($url == "contact" || $url == "contact") echo "class='active'"; ?>><a href="/contact">Contact</a></li>
              </ul>
-              <ul id="main-nav" class="nav navbar-nav custom-nav navbar-right">
+           	 		   			 SPONSORS             	EVENTS	     	        NEWS   	         
+                
+              <ul id="main-nav" class="nav navbar-nav navbar-right">
                 <li class="dropdown new-search-dropdown">
                   <a href="#" title="search button" class="dropdown-toggle search-button" data-toggle="dropdown">&nbsp;</a>
                   <ul class="dropdown-menu" role="">
