88fd5180e6c66f47a74afe7dc981dc9321f3f448
JJcodesinDC <josh@innuevodigital.com>
2015-08-08 00:29:45 -0400

    updates for issues box

--- wp-content/themes/DCBIA/front-page.php
+++ wp-content/themes/DCBIA/front-page.php
@@ -61,21 +61,23 @@ get_header(); ?>
 <p>&nbsp;</p>
     <div class="container all-pad-gone issues">
         <div class="row">
-            <div class="col-md-8">   
-                <h2>ISSUES<h2>
-            </div> 
-                <div class="col-md-4">
-                    <a href="">
-                        <img class="img-responsive" src="<?php echo get_template_directory_uri() ;?>/img/featured-building.jpg" alt="sail boat on water" />
-                        <span class="infoblock3 light-blue">WE KEEP YOU UPDATED...</span>
-                    </a>
-                </div>
-                <div class="col-md-4">
-                    <a href="">
-                        <img class="img-responsive" src="<?php echo get_template_directory_uri() ;?>/img/featured-building.jpg" alt="sail boat on water" />
-                        <span class="infoblock3 light-blue">WE KEEP YOU UPDATED...</span>
-                    </a>
-                </div>
+            <div class="col-md-8 all-pad-gone"> 
+                    <div class="col-md-12">
+                        <h2>ISSUES<h2>
+                    </div>    
+                    <div class="col-md-6">
+                        <a href="">
+                            <img class="img-responsive" src="<?php echo get_template_directory_uri() ;?>/img/featured-building.jpg" alt="sail boat on water" />
+                            <span class="infoblock3 light-blue">WE KEEP YOU UPDATED...</span>
+                        </a>
+                    </div>
+                    <div class="col-md-6">
+                        <a href="">
+                            <img class="img-responsive" src="<?php echo get_template_directory_uri() ;?>/img/featured-building.jpg" alt="sail boat on water" />
+                            <span class="infoblock3 light-blue">WE KEEP YOU UPDATED...</span>
+                        </a>
+                    </div>
+                </div>    
                  <div class="col-md-4">
                     <div class="">
                         <h3>Systems-Oriented Evaluation</h3>
