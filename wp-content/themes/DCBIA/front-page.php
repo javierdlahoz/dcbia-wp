40c101472eda43bd1a126157a122ff600a312ea9
JJcodesinDC <josh@innuevodigital.com>
2015-08-11 14:34:52 -0400

    update for issues iput email

--- wp-content/themes/DCBIA/front-page.php
+++ wp-content/themes/DCBIA/front-page.php
@@ -87,7 +87,7 @@ get_header(); ?>
                         <form id="signup-form" action="" method="">
                             <label class="hidden" for="email"></label>
                             <input id="email1" class="" name="email" type="text" placeholder="Email Address" />
-                            <input id="submit-btn" type="submit" value="Submit" />
+                            <input id="submit-btn" type="submit" value="SUBMIT" />
                         </form>						
                     </div>
                 </div>
