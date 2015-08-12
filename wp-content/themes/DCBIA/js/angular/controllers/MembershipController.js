b3056d7722951008b28aa938c86bc8beb766a652
Javier De la Hoz <javierdlahoz@gmail.com>
2015-08-11 22:59:00 -0500

    register page using ajax and angular

--- wp-content/themes/DCBIA/js/angular/controllers/MembershipController.js
+++ wp-content/themes/DCBIA/js/angular/controllers/MembershipController.js
@@ -2,16 +2,19 @@ angular.module('angular-wp')
     .controller('MembershipController', function ($scope, $http) {
 
     	$scope.users = {};
-    	$scope.c = 0;
+    	$scope.c = -1;
+    	$scope.usernameTaken = false;
+    	$scope.isSuccess = false;
     	
     	$scope.add = function(){
     		$scope.c ++;
     		var index = $scope.c; 
     		$scope.users[index] = {
 				id: index,
-    			fname: "",
-				lname: "",
+    			first_name: "",
+				last_name: "",
 				email: "",
+				password: "",
 				committee: ""
     		};
     	};
@@ -21,13 +24,43 @@ angular.module('angular-wp')
     	};
     	
     	$scope.initialize = function(){
-    		$scope.users[$scope.c] = {
-				id: "0",
-    			fname: "",
-				lname: "",
-				email: "",
-				committee: ""
+    		$scope.users = {
     		};
+    	};
+    	
+    	$scope.setCity = function(){
+    		if (typeof $scope.member.city == "object"){
+    			$scope.member.state = $scope.member.city.address_components[1].short_name;
+    			$scope.member.city = $scope.member.city.address_components[0].long_name;
+    		}
+    	};
+    	
+    	$scope.register = function(){
+    		var url = "/api/member/register";
+    		$scope.member.additional_users = $scope.users;
+    		$http({
+                url: url,
+                method: "POST",
+                data: jQuery.param($scope.member),
+                headers: getContentTypes().form
+            }).success(function (data) {
+            	$scope.isSuccess = true;
+            });
+    		
+    	};
+    	
+    	$scope.isUsernameTaken = function(){
+    		if($scope.member.username.length > 4){
+    			var url = "/api/member/check";
+        		$http({
+                    url: url,
+                    method: "POST",
+                    data: jQuery.param({username: $scope.member.username}),
+                    headers: getContentTypes().form
+                }).success(function (data) {
+                	$scope.usernameTaken = data.is_taken;
+                });
+    		}
     	};
     
     	angular.element("#address").attr("g-places-autocomplete", "");
--- wp-content/themes/DCBIA/page-register.php
+++ wp-content/themes/DCBIA/page-register.php
@@ -1,7 +1,9 @@
 <?php
 use INUtils\Entity\PostEntity;
 use Committee\Controller\CommitteeController;
+use Member\Controller\MemberController;
 $pageEntity = new PostEntity(get_the_ID());
+
 get_header(); ?>
 <p>&nbsp;</p>
 <div class="container inside-pages">
@@ -13,37 +15,126 @@ get_header(); ?>
     <div class="container inside-pages" ng-controller="MembershipController">
         <div class="row">
             <div class="col-md-12">
-                <form method="post">
-                    <input type="text" name="query">
-                    <input type="submit" value="Search">
-                    <input type="hidden" name="action-type" value="search">
-                    <input type="hidden" name="resource-types[]" id="resource-types[]" value="Evaluation Reports">
-                </form>
-
                 <h3><?php echo $pageEntity->getTitle(); ?></h3>
-                <?php 
-                    echo do_shortcode('[pmpro_signup level="3" short="1" title="Sign Up for Gold Membership" intro="0" button="Signup Now"]'); 
-                ?>
-                <div ng-init="initialize()">
+                
+                <div ng-show="isSuccess">
+                    <div class="alert alert-success">The membership was created successfully</div>
+                </div>
+                
+                <form class="form-horizontal" ng-init="initialize()" ng-hide="isSuccess">
+                    <div class="form-group">
+                      <label>First Name</label>
+                      <input type="text" class="form-control" placeholder="First name" ng-model="member.first_name" required>
+                    </div>
+                    <div class="form-group">
+                      <label>Last Name</label>
+                      <input type="text" class="form-control" placeholder="Last name" ng-model="member.last_name" required>
+                    </div>
+                    <div class="form-group">
+                        <label>Username</label>
+                        <input type="text" class="form-control" placeholder="Username" ng-model="member.username" 
+                            required ng-change="isUsernameTaken()">
+                        <div ng-show="usernameTaken">
+                            <br>
+                            <div class="alert alert-danger">This username is already taken</div>
+                        </div>
+                        
+                    </div>
+                    <div class="form-group">
+                      <label>Password</label>
+                      <input type="password" class="form-control" placeholder="Password" ng-model="member.password" required>
+                    </div>
+                    <div class="form-group">
+                      <label>Email</label>
+                      <input type="email" class="form-control" placeholder="Email" ng-model="member.email" required>
+                    </div>
+                    <div class="form-group">
+                      <label>Address</label>
+                      <input type="text" class="form-control" placeholder="Address" ng-model="member.address1">
+                    </div>
+                    <div class="form-group">
+                      <label>Address2</label>
+                      <input type="text" class="form-control" placeholder="Address2" ng-model="member.address2">
+                    </div>
+                    <div class="form-group">
+                      <label>Address3</label>
+                      <input type="text" class="form-control" placeholder="Address3" ng-model="member.address3">
+                    </div>
+                    <div class="form-group">
+                      <label>City</label>
+                      <input type="text" class="form-control" placeholder="City" ng-model="member.city" 
+                        g-places-autocomplete ng-change="setCity()">
+                    </div>
+                    <div class="form-group">
+                      <label>State</label>
+                      <input type="text" class="form-control" placeholder="State" ng-model="member.state">
+                    </div>
+                    <div class="form-group">
+                      <label>ZIP</label>
+                      <input type="number" class="form-control" placeholder="ZIP" ng-model="member.zip">
+                    </div>
+                    <div class="form-group">
+                      <label>Telephone</label>
+                      <input type="number" class="form-control" placeholder="Telephone" ng-model="member.telephone">
+                    </div>
+                    <div class="form-group">
+                      <label>Referred By</label>
+                      <input type="text" class="form-control" placeholder="Referred By" ng-model="member.refered_by">
+                    </div>
+                    <div class="form-group">
+                      <label>Company Name</label>
+                      <input type="text" class="form-control" placeholder="Company Name" ng-model="member.company_name">
+                    </div>
+                    <div class="form-group">
+                      <label>Company Website</label>
+                      <input type="url" class="form-control" placeholder="Company Website" ng-model="member.company_website">
+                    </div>
+                    <div class="form-group">
+                      <label>Company Description</label>
+                      <textarea class="form-control" ng-model="member.company_description"></textarea>
+                    </div>
+                    
                     <h2>Affiliate Listing in Directory</h2>
-                    <p>Membership in DCBIA is corporate based and it entitles you to one representative to be listed in the Membership Directory under your company’s listing. Additional representatives from a member firm can be listed as affiliate members for a $75/year charge.</p>
+                    <p>Membership in DCBIA is corporate based and it entitles you to one representative to be listed in the Membership 
+                    Directory under your company’s listing. Additional representatives from a member firm can be listed as 
+                    affiliate members for a $75/year charge.</p>
                     <p><b>Enter information for Affiliates to be added * :</b></p>
                     <p>($ 75 per individual)</p>
                     <div class="user-container">
-                        <div class="input-group" ng-repeat="user in users">
-                              <input type="text" class="form-control" placeholder="First name" ng-model="user.fname">
-                              <input type="text" class="form-control" placeholder="Last name" ng-model="user.lname">
-                              <input type="email" class="form-control" placeholder="E-Mail" ng-model="user.email">
-                              <select class="form-control" ng-model="user.committee">
-                                <?php foreach (CommitteeController::getSingleton()->getCommitteeTerms() as $committee): ?>
-                                    <option value="<?php echo $committee->getTermId(); ?>"><?php echo $committee->getName(); ?></option>
-                                <?php endforeach; ?>
-                              </select>
-                              <button class="btn btn-danger" ng-click="remove(user.id)" ng-show="$index > 0">Remove Affiliate</button>
+                        <div class="form-horizontal">
+                            <div ng-repeat="user in users">
+                                <div class="form-group">
+                                    <label>First name</label>
+                                    <input type="text" class="form-control" placeholder="First name" ng-model="user.first_name">
+                                </div>
+                                <div class="form-group">
+                                    <label>Last name</label>
+                                    <input type="text" class="form-control" placeholder="Last name" ng-model="user.last_name">
+                                </div>
+                                <div class="form-group">
+                                    <label>E-Mail</label>
+                                    <input type="email" class="form-control" placeholder="E-Mail" ng-model="user.email">
+                                </div>
+                                <div class="form-group">
+                                    <label>Password</label>
+                                    <input type="password" class="form-control" placeholder="Password" ng-model="user.password">
+                                </div>
+                                <div class="form-group">
+                                    <label>Committee</label>
+                                    <select class="form-control" ng-model="user.committee">
+                                    <?php foreach (CommitteeController::getSingleton()->getCommitteeTerms() as $committee): ?>
+                                        <option value="<?php echo $committee->getTermId(); ?>"><?php echo $committee->getName(); ?></option>
+                                    <?php endforeach; ?>
+                                    </select>
+                                </div>
+                                <div class="btn btn-danger" ng-click="remove(user.id)">Remove Affiliate</div>
+                            </div>
+                            <div class="btn btn-primary" ng-click="add()">Add Affiliate</div>
                         </div>
-                        <button class="btn btn-primary" ng-click="add()">Add Affiliate</button>
                     </div>
-                </div>
+                    <br>
+                    <button class="btn btn-success" ng-disabled="usernameTaken" ng-click="register()">Send</button>
+                </form>
                 <br>
             </div>
         </div>
