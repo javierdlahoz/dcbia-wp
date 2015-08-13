034a33f9c4b84261a784e493c1ead7e678b78f56
JJcodesinDC <josh@innuevodigital.com>
2015-08-13 18:10:12 -0400

    update for footer styles

--- wp-content/themes/DCBIA/footer.php
+++ wp-content/themes/DCBIA/footer.php
@@ -18,44 +18,38 @@
 	<div class="container all-pad-gone">
 		<div class="col-md-4">
             <p>&nbsp;</p>
-			<img class="img-responsive"
-				src="<?php echo get_template_directory_uri() ;?>/img/dcbia-white.png" />
+			<img class="img-responsive" src="<?php echo get_template_directory_uri() ;?>/img/dcbia-white.png" />
 		</div>
+        
 		<div class="col-md-4">
 			<p>&nbsp;</p>
-			<h6>
-				<b>Our Mission</b>
-			</h6>
-			<p>InSites, a Colorado-based nonprofit organization, promotes learning, growth, and change through inquiry and collaboration. We help people build relationships, and change social systems based on results, values, and attention to a desired and sustainable, fair future for all.</p>
-			
+			<h4>Address</h4>
+			<p>District of Columbia Building Industry Association<br> 
+                455 Massachusetts Avenue,<br>
+                NW, Suite 400<br> 
+                Washington, D.C. 20001
+            </p>
 		</div>
+        
 		<div class="col-md-4 no-border">
 			<p>&nbsp;</p>
-			<h6>
-				<b>Contact us!</b>
-			</h6>
-            <p>
-				 	<span>Address: 1307 Sanford Drive,<br> Fort
-					Collins, CO 80526</span> <span>Phone: 970-226-1003</span>
-			<a href="mailto:bparsons@insites.org">Email: Beverly Parsons | bparsons@insites.org</a>
-			</p>
+			<h4>Contact</h4>
+            <p>Telephone:  (202) 966-8665</p> 
+            <p>Fax:  (202) 966-3222</p>
+            <p><a href="mailto:info@dcbia.org">Email: info@dcbia.org</a></p>
 
-<!--			<div class="small-social-icons">
-				<a href='/contact/' class="" href="mailto:"><img
-					src="<?php echo get_template_directory_uri() ;?>/img/email-small.png" /></a>
-				 <a class="" href=""><img
-					src="<?php echo get_template_directory_uri() ;?>/img/twitter-small.png" /></a>
-				<a class="" href=""><img
-					src="<?php echo get_template_directory_uri() ;?>/img/facebook-small.png" /></a>
-				<a class="" href=""><img
-					src="<?php echo get_template_directory_uri() ;?>/img/linkedin-small.png" /></a>
+			<div class="small-social-icons">
+				<a class="" href="" href="mailto:"></a>
+				<a class="" href=""></a>
+				<a class="" href=""></a>
 				<p style="clear: both;">&nbsp;</p>
 			</div> 
-		</div>-->
+		</div>
 	</div>
 	<div class="container">
 		<div class="col-md-12">
-			<p class="centered">&copy; 2015 InSites All Rights Reserved</p>
+            <p>&nbsp;</p>
+			<p class="centered">&copy; 2015 District of Columbia Building Industry Association.<br> All Rights Reserved.</p>
 		</div>
 		<p>&nbsp;</p>
 	</div>
