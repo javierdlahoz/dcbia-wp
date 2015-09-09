30d48fa916f153bc490371b88b798ce4f7c236f0
JJcodesinDC <josh@innuevodigital.com>
2015-09-09 11:55:02 -0400

    updates for disabled button on register

--- wp-content/themes/DCBIA/page-register.php
+++ wp-content/themes/DCBIA/page-register.php
@@ -190,7 +190,7 @@ get_header(); ?>
                     </div>
                 </div>
                 <div class="col-md-12">
-                    <button class="button3 disabled1" ng-disabled="disabledToSend" ng-click="register()">Submit Registration Form</button>
+                    <button class="button3" ng-disabled="disabledToSend" ng-click="register()">Submit Registration Form</button>
                 </div>    
             </form>
         </div> 
