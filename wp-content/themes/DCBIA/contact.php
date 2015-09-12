d1fb6553b729fe5158a9810ac9215ccce0790b9e
JJcodesinDC <josh@innuevodigital.com>
2015-09-12 15:50:23 -0400

    updates for contact map

--- wp-content/themes/DCBIA/contact.php
+++ wp-content/themes/DCBIA/contact.php
@@ -10,58 +10,43 @@ get_header(); ?>
     <div class="row">
         <div class="col-md-12">
             <h2>Contact</h2>
+            <br>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3105.003818272561!2d-77.017944!3d38.901028!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b7b7946b379afd%3A0xe85a9d9a21f3a1b0!2s455+Massachusetts+Avenue%2C+Washington%2C+DC+20001!5e0!3m2!1sen!2sus!4v1442085046430" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
         </div>    
     </div>    
-</div> 
+</div>
+<p>&nbsp;</p>
 <div class="container all-pad-gone">
-        <div class="row">
-            <div class="col-sm-6" id="contact-form">
-                <h4>Contact DCBIA</h4>
-                <div class="form-group">
-                    <label for="name">Name</label>
-                    <input class="form-control" type="text" name="name" id="name">
-                </div>
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input class="form-control" type="email" name="from" id="from">
-                </div>
-                <div class="form-group">
-                    <label for="comment">Comment</label>
-                    <textarea name="content" cols="30" rows="10" style="resize:none; width:100%;" id="content"></textarea>
-                </div>
-                <input type="hidden" name="send_email">
-                <input type="hidden" name="to" id="to" value="trevor@innuevodigital.com">
-                <input class="btn btn-default" type="submit" name="submit" value="Send" onclick="sendContactEmail();">
+    <div class="row">
+        <div class="col-sm-6" id="contact-form">
+            <h4>Contact DCBIA</h4>
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input class="form-control" type="text" name="name" id="name">
             </div>
-            <div class="col-sm-6" id="contact-form-sent" style="display: none">
-                <h3>Your email was sent</h3>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input class="form-control" type="email" name="from" id="from">
             </div>
-            <div class="col-sm-6">
+            <div class="form-group">
+                <label for="comment">Comment</label>
+                <textarea name="content" cols="30" rows="10" style="resize:none; width:100%;" id="content"></textarea>
+            </div>
+            <input type="hidden" name="send_email">
+            <input type="hidden" name="to" id="to" value="trevor@innuevodigital.com">
+            <input class="button2" type="submit" name="submit" value="Send" onclick="sendContactEmail();">
+        </div>
+        <div class="col-sm-6" id="contact-form-sent" style="display: none">
+            <h3>Your email was sent</h3>
+        </div>
+        <div class="col-sm-6">
         	<h4>Info</h4>
-            <p>202-537-1107<br> 
-            <a href="mailto:studio@marshallmoya.com">studio@marshallmoya.com</a><br> 
-            2201 Wisconsin Avenue, NW  Suite 305 Washington, D.C. 20007</p>
-            <br>
-        	<div class="container-fluid information">
-                <div class="row-fluid">
-                    <div class="col-md-4">
-                        <h6>Architecture/ Masterplan/ Interiors</h6>
-                        <p>Paola Moya</p>
-                        <a href="mailto:paola@marshallmoya.com">paola@marshallmoya.com</a>
-                    </div>
-                    <div class="col-md-4">
-                        <h6>Business Development</h6>
-                        <p>Craig Dean</p>
-                        <a href="mailto:craig@marshallmoya.com">craig@marshallmoya.com</a>
-                    </div>
-
-                    <div class="col-md-4">
-                        <h6>Operations/ HR/ Proposals</h6>
-                        <p>Jenny Lopez</p>
-                        <a href="mailto:jenny@marshallmoya.com">jenny@marshallmoya.com</a>
-                    </div>  
-        	   </div>
-        	</div>
+                <p>Telephone: (202) 966-8665</p> 
+                <p>Fax: (202) 966-3222</p> 
+            <p><a href="mailto:info@dcbia.org">info@dcbia.org</a></p>
+            <p>District of Columbia Building Industry Association<br>
+                455 Massachusetts Avenue, NW, Suite 400<br> 
+                Washington, D.C. 20001</p>
         </div>
     </div>
 </div>
