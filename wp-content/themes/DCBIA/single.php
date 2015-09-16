36f6e7e2257b11c56741d0b1cd03fd1e98fa90ae
Javier De la Hoz <javierdlahoz@gmail.com>
2015-09-16 11:49:59 -0500

    redirecting to abut page

--- wp-content/themes/DCBIA/single.php
+++ wp-content/themes/DCBIA/single.php
@@ -1,6 +1,14 @@
 <?php 
 use Member\Helper\MemberHelper;
-if(!MemberHelper::isCurrentAccountActive()){
+use INUtils\Entity\PostEntity;
+use Staff\Helper\StaffHelper;
+
+$p = new PostEntity(get_the_ID());
+
+if($p->getType() == StaffHelper::STAFF_POST_TYPE){
+    header('location: /about/staff');
+}
+elseif(!MemberHelper::isCurrentAccountActive()){
     header('location: /renewal');
 }
 get_header(); ?>
