f256b777d9dfd8646f995dcfc97b3b6a365ba22c
Javier De la Hoz <javierdlahoz@gmail.com>
2015-09-17 11:14:02 -0500

    blog single page

--- wp-content/themes/DCBIA/single-blog.php
+++ wp-content/themes/DCBIA/single-blog.php
@@ -1,37 +1,36 @@
 <?php
 use INUtils\Entity\PostEntity;
-use Staff\Controller\StaffController;
-/*
-  Template Name: single-blog
-*/
-$pageEntity = new PostEntity(get_the_ID());
+
+$p = new PostEntity(get_the_ID());
 get_header();
 ?>
 <div class="container all-pad-gone">
     <?php echo do_shortcode('[slideshow group="blog"]'); ?>
     <?php echo getTopMenu(); ?>       
 </div> 
-
-<!--start main content here-->
-
 <div class="container all-pad-gone">
     <div class="row">
         <div class="col-md-12 about">   
-            <h2><?php echo $pageEntity->getTitle(); ?></h2>
-            <p><?php echo $pageEntity->getContent(); ?></p>
+            <h2><?php echo $p->getTitle(); ?></h2>
         </div>    
     </div>
  <br>
     <div class="row">
         <div class="col-md-4">
-            <img class="img-responsive" alt="" src="<?php echo get_template_directory_uri() ;?>/img/place-holder.jpg" alt="Property Group Partners logo" />
+            <img class="img-responsive" alt="" src="<?php if($p->getImage() == "")
+            { 
+                echo get_template_directory_uri()."/img/place-holder.jpg"; 
+            }
+            else{
+                echo $p->getImage();
+            }
+            ?>" 
+            alt="Property Group Partners logo" />
         </div>
         <div class="col-md-8 key-info">
-            <h4>Title of Post</h4>
-            <h5>Date of Post</h5>  
-            <p>Content of Post</p>
+            <h5><?php echo mysql2date("j F, Y", $p->getDate()); ?></h5>  
+            <p><?php echo $p->getContent(); ?></p>
         </div>
     </div>
-
 </div>
 <?php get_footer(); 
--- wp-content/themes/DCBIA/single.php
+++ wp-content/themes/DCBIA/single.php
@@ -9,6 +9,10 @@ $p = new PostEntity(get_the_ID());
 if($p->getType() == StaffHelper::STAFF_POST_TYPE){
     header('location: /about/staff');
 }
+elseif($p->getType() == "post"){
+    require_once __DIR__.'/single-blog.php';
+    exit();
+}
 elseif(!MemberHelper::isCurrentAccountActive()){
     header('location: /renewal');
 }
