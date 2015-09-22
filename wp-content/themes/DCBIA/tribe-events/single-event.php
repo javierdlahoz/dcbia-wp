8443769018c772b51d1fa09da24d2f42b0b80499
Javier De la Hoz <javierdlahoz@gmail.com>
2015-09-22 10:34:33 -0500

    single event

--- wp-content/themes/DCBIA/tribe-events/single-event.php
+++ wp-content/themes/DCBIA/tribe-events/single-event.php
@@ -37,36 +37,7 @@ $event_id = get_the_ID();
     </div>
     
 </div>
-
-	<?php /*
-	   while ( have_posts()) :  the_post(); ?>
-		<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			<!-- Event featured image, but exclude link -->
-			<?php echo tribe_event_featured_image( $event_id, 'full', false ); ?>
-
-			<!-- Event content -->
-			<?php do_action( 'tribe_events_single_event_before_the_content' ) ?>
-			<div class="tribe-events-single-event-description tribe-events-content entry-content description">
-				<?php the_content(); ?>
-			</div>
-			<!-- .tribe-events-single-event-description -->
-			<?php do_action( 'tribe_events_single_event_after_the_content' ); ?>
-
-			<!-- Event meta -->
-			<?php do_action( 'tribe_events_single_event_before_the_meta' ) ?>
-			<?php
-				tribe_get_template_part( 'modules/meta' );			
-			?>
-			<?php do_action( 'tribe_events_single_event_after_the_meta' ) ?>
-		</div> <!-- #post-x -->
-		<?php if ( get_post_type() == Tribe__Events__Main::POSTTYPE && tribe_get_option( 'showComments', false ) ) comments_template() ?>
-	<?php endwhile; */ ?>
-
-	
-	<!-- #tribe-events-footer -->
-
-</div><!-- #tribe-events-content -->
 <script type="text/javascript">
 	jQuery(jQuery(".date-start.dtstart")[0]).hide();
-	jQuery(jQuery(".event-details")[0]).hide()
+	jQuery(jQuery(".event-details")[0]).hide();
 </script>
