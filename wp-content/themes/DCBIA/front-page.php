01a1fa9d7df5b4825649ef9a0bd8c31971e0e70d
JJcodesinDC <josh@innuevodigital.com>
2015-09-24 08:57:45 -0400

    updates for sponsors box

--- wp-content/themes/DCBIA/front-page.php
+++ wp-content/themes/DCBIA/front-page.php
@@ -134,7 +134,7 @@ get_header();
                         <p class="news-date"><?php echo mysql2date('F j, Y', $news->getDate()); ?></p>
                         <p><?php echo TextHelper::cropText($news->getContent(), 400); ?></p>
                     </div>
-                    <hr>
+                    
                     <?php if($isFirst): $isFirst = false; ?>
                         
                     <?php endif; ?>
