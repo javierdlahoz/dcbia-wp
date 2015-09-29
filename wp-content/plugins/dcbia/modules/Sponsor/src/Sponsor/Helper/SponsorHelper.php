6a41ab528e121886c418433d5e9d50e7d5010929
Javier De la Hoz <javierdlahoz@gmail.com>
2015-09-29 09:36:31 -0500

    sponsors type

--- wp-content/plugins/dcbia/modules/Sponsor/src/Sponsor/Helper/SponsorHelper.php
+++ wp-content/plugins/dcbia/modules/Sponsor/src/Sponsor/Helper/SponsorHelper.php
@@ -3,16 +3,19 @@ namespace Sponsor\Helper;
 
 use INUtils\Helper\PostHelper;
 use Sponsor\Controller\SponsorController;
+use INUtils\Helper\TaxonomyHelper;
 
 class SponsorHelper
 {
     const SINGULAR = "Sponsor";
     const PLURAL = "Sponsors";
     const POST_TYPE = "sponsor";
+    const TAXONOMY =  "sponsor_type";
 
     function __construct(){
         $this->sponsorController = SponsorController::getSingleton();
         $this->createSponsorPostType();
+        $this->createTaxonomy();
 
         add_action('add_meta_boxes_sponsor', array(&$this, 'addMetaBoxesForSponsor'));
         add_action('save_post', array(&$this->sponsorController, 'save'));
@@ -37,5 +40,25 @@ class SponsorHelper
 
     public function getMetaBoxForMoreInfo(){
         include __DIR__."/../views/admin/edit-form.php";
+    }
+    
+    public function createTaxonomy(){
+        $labels = array(
+            'name'              => "Sponsor Type",
+            'singular_name'     => "Sponsor Type",
+            'update_item'       => "Update Sponsor Type",
+            'add_new_item'      => "Add a new Sponsor Type",
+            'new_item_name'     => "New Sponsor Type Name",
+        );
+    
+        register_taxonomy(
+            self::TAXONOMY,
+            array(self::POST_TYPE),
+            array( 'hierarchical' => true,
+                'labels' => $labels,
+                'query_var' => self::TAXONOMY,
+                'rewrite' => array('slug' => self::TAXONOMY)
+            )
+        );
     }
 }
