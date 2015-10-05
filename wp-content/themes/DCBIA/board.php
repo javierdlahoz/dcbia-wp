1153f25ba848eb2ac195e1f7af098fa5a8b3a6cb
JJcodesinDC <josh@innuevodigital.com>
2015-10-05 14:01:53 -0400

    updates for the board page

--- wp-content/themes/DCBIA/board.php
+++ wp-content/themes/DCBIA/board.php
@@ -12,9 +12,6 @@ get_header();
     <?php echo do_shortcode('[slideshow group="about"]'); ?>
     <?php echo getTopMenu(); ?>       
 </div> 
-
-<!--start main content here-->
-
 <div class="container all-pad-gone">
     <div class="row">
         <div class="col-md-12 about">   
@@ -22,8 +19,19 @@ get_header();
             <p><?php echo $pageEntity->getContent(); ?></p>
         </div>    
     </div>
-
+    <div class="row about">
+        <div class="col-sm-2 staff-pic board-members">
+             <img class="img-responsive" src="<?php echo get_template_directory_uri() ;?>/img/place-holder.jpg" alt="Board memeber" />
+            <h4 class="heading-top-space">Place Holder</h4>
+            <h5>Place holder</h5>  
+            <p>some text for a placeholder</p>
+        </div>    
+    </div>        
+    <br>
     <div class="row">
+        <div class="col-md-12 about">
+            <h4>DOWNLOAD THE FOLLOWING PDF'S:</h4>
+        </div>
         <?php foreach ($boards as $board): ?>
         <div class="col-md-12 board">
             <a class="button2" href="<?php echo $board->getFileUrl(); ?>">
