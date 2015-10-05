9737a9877fd04e5702aa2fddb80dcca4e4ca90f5
Javier De la Hoz <javierdlahoz@gmail.com>
2015-10-05 15:06:17 -0500

    no email for board member

--- wp-content/plugins/dcbia/modules/Board/src/Board/views/admin/edit-form.php
+++ wp-content/plugins/dcbia/modules/Board/src/Board/views/admin/edit-form.php
@@ -15,15 +15,6 @@ $boardEntity = dcbia::getEntity("board");
 				required="true" />
 		</div>
 	</div>
-	<div class="form-group">
-		<label class="control-label col-lg-2 col-sm-2">Email</label>
-		<div class="col-lg-10 col-sm-10">
-			<input type="email" class="form-control"
-				name="board_email" id="board_email"
-				value="<?php echo $boardEntity->getEmail(); ?>"
-				required="true" />
-		</div>
-	</div>
 </div>
 <?php
 echo PostHelper::addStylesAndScripts();
